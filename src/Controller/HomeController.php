8c916180d5f68513bdc2268be959412018a3a0f1
albertaljeanpierre <albertaljeanpierre@gmail.com>
2022-12-26 12:22:08 +0100

    Gestion du formulaire des prestataires

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,11 +11,14 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
+     * @param Request $request la requête POST pour la recherche des prestataires
+     * @return Response
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+       // dump($request);
         return $this->render('home/index.html.twig', [
-
+            'requete' => $request
         ]);
     }
 }
--- src/Controller/Recherche/RechercheController.php
+++ src/Controller/Recherche/RechercheController.php
@@ -10,12 +10,37 @@ use Symfony\Component\HttpFoundation\Response;
 class RechercheController extends AbstractController
 {
 
-    public function recherchePrestataire(Request $request)
+    public function recherchePrestataire($request): Response
     {
-        $response = "Un texte de réponse";
+        // $request est transmit en paramètre de la fonction depuis base.html.twig qui récupère la requête de Request
+        // Ce qui permet de récupérer les données de POST dans ce controller
 
+//        dump($request );
+        $data = null;
+        $response = "";
+        if ($request->request->count() > 0) { // Si il y a des données envoyées en POST
+            function my_empty( $arr)
+            {
+                foreach ($arr as $key => $value) {
+                    if ($value ) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            if (my_empty($request->request)) {
+                $response = "Recherche de tous les prestataires";
+
+            } else {
+                $response = "Recherche selon les donnés fournie";
+                $data = $request->request;
+            }
+            //dump($request->request);
+        }
         return $this->render('recherche/recherchePrestataire.html.twig', [
-                'response' => $response
+            'data' => $data,
+            'response' => $response
         ]);
     }
 }
