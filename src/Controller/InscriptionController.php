14592bc21043ef69dcc17fe52634ffcabf976113
albertal jean-pierre <albertaljeanpierre@gmail.com>
2023-02-10 16:57:04 +0100

    insertion de donnée d'inscription en db

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,7 +27,7 @@ class InscriptionController extends AbstractController
                 $subject = "Inscription à l'annuaire du bien-être";
                 $message = "Bonjour, suite à votre inscription à l'annuaire du bien-être, veuillez confirmer votre inscription en cliquant sur le lien suivant:";
                 $token = sha1($to);
-                $lien = 'http://' . $_SERVER['HTTP_HOST'] . "inscription/confirmation?mail=" . urlencode($to) . '&amp;token=' . $token;
+                $lien = 'http://' . $_SERVER['HTTP_HOST'] . "/inscription/confirmation?mail=" . urlencode($to) . '&amp;token=' . $token;
 
                 $message .= '<br><br><a href="' . $lien . '">Valider mon inscription</a>';
                 // Pour envoyer un mail HTML, l'en-tête Content-type doit être défini
@@ -54,8 +57,36 @@ class InscriptionController extends AbstractController
 
 
     #[Route('/inscription/confirmation', name: 'app_inscription_confirmation')]
-    function confirmationInscription()
+    function confirmationInscription(Request $request, EntityManagerInterface $entityManager )
     {
+        // dd($request->query->get('mail'));
+         //dd($request);
+        $mail = $request->query->get('mail');
+        $token = $request->query->get('token');
+        $tokenVerif = sha1($mail);
+        $mailVerif = filter_var($mail, FILTER_VALIDATE_EMAIL);
+        if ( ($token === $tokenVerif) and ( $mailVerif !== false) ) { // insertion mail en base si l'email n'existe pas encore en base
 
+            $userRepo = $entityManager->getRepository(User::class);
+            $userMail = $userRepo->findOneBy(["email" => $mailVerif]);
+            //dd($userMail );
+            if(is_null($userMail )) { // L'utilisateur ne s'est pas encore inscrit
+                $user = new  User();
+                $user->setInscription( new \DateTime());
+                $user->setInscriptionConfirmee(true);
+                $user->setEmail($mailVerif);
+                $entityManager->persist($user);
+                $entityManager->flush();
+            } else { // l'utilisateur est déjà inscrit
+                dd('erreur utilisateur déjà inscrit');
+
+            }
+
+        } else { // affichage message d'erreur si lien corrompu
+            return $this->render('inscription/erreurInscription.html.twig', [
+
+            ]);
+
+        }
     }
 }
